6cd7dcda2eaecbcb09cb8a0424fac0840f753cc1
federico_viganò <fede_viga@hotmail.it>
2024-02-28 17:20:22 +0100

    aggiunta immagine - fine esercizio

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -3,10 +3,21 @@
 import AppTitle from './AppTitle.vue';
 
 export default {
+
     name: 'AppHero',
     components: {
         AppTitle,
+    },
+
+    data() {
+        return {
+
+            image: "https://img.freepik.com/free-photo/matrix-hacker-background_23-2150082005.jpg?size=626&ext=jpg&ga=GA1.1.1788068356.1706140800&semt=ais"
+
+        }
     }
+
+
 }
 
 </script>
@@ -17,16 +28,30 @@ export default {
 
         <AppTitle></AppTitle>
 
-        <h2>
-            > Benvenuto nella mia prima App creata con Vite! Purtroppo quest'app non fa ancora niente :(
-        </h2>
+        <div class="row">
+        
+            <div class="col">
 
-        <div class="buttons">
+                <h2>
+                    > Benvenuto nella mia prima App creata con Vite! Purtroppo quest'app non fa ancora niente :(
+                </h2>
+                
+                <div class="buttons">
+        
+                    <a href="#"> > Link che non porta da nessuna parte</a>
+            
+                    <div class="button">
+                        > Bottone che non fa niente
+                    </div>
+        
+                </div>
 
-            <a href="#"> > Link che non porta da nessuna parte</a>
-    
-            <div class="button">
-                > Bottone che non fa niente
+            </div>
+
+            <div class="col">
+
+                <img :src="image" alt="immagine">
+                
             </div>
 
         </div>
@@ -46,6 +71,12 @@ export default {
 
 h2 {
     margin-bottom: 20px;
+}
+
+.row {
+    display: flex;
+    justify-content: center;
+    gap: 50px;
 }
 
 .buttons {
@@ -75,6 +106,10 @@ a:hover {
     transform: scale(1.05);
     background-color: lime;
     color: black;
+}
+
+img {
+    width: 300px;
 }
 
 </style>
